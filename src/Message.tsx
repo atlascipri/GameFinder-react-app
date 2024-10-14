dc30c2737d7d5768582ebeb3d568c5231219a34d
atlascipriano <atlasciprianoo@gmail.com>
2024-10-14 02:14:30 -0300

    feat: cria primeiro componente Message

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing - functions
+function Message() {
+    // JSX: Javascript XML - 
+    const name =  'Atlas';
+    if (name)
+        return <h1> Hello {name} </h1>;
+    return <h1> Hello World </h1>
+}
+
+export default Message;
